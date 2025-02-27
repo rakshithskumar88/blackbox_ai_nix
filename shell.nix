217628a827f62652f75e557fb92fa8c6f9110876
CodeSandbox <support@codesandbox.io>
2025-02-27 13:15:32 +0000

    Simplify shell.nix with bundled Python environment and core GTK dependencies

--- shell.nix
+++ shell.nix
@@ -1,16 +1,19 @@
 { pkgs ? import <nixpkgs> {} }:
 
+let
+  pythonEnv = pkgs.python3.withPackages (ps: with ps; [
+    pip
+    virtualenv
+    pygobject3
+    pycairo
+  ]);
+in
 pkgs.mkShell {
   buildInputs = with pkgs; [
-    python3
-    python3Packages.pip
-    python3Packages.virtualenv
-    python3Packages.pygobject3
-    python3Packages.pycairo
+    pythonEnv
     gtk3
     gtk3.dev
     gobject-introspection
-    gobject-introspection.dev
     pkg-config
     cairo
     cairo.dev
@@ -22,15 +25,23 @@ pkgs.mkShell {
     ninja
   ];
 
+  nativeBuildInputs = with pkgs; [
+    gobject-introspection
+  ];
+
   shellHook = ''
     # Set up environment variables
-    export GI_TYPELIB_PATH="${pkgs.gtk3}/lib/girepository-1.0:${pkgs.gtk3.dev}/lib/girepository-1.0:${pkgs.pango}/lib/girepository-1.0:${pkgs.pango.dev}/lib/girepository-1.0:${pkgs.glib}/lib/girepository-1.0:${pkgs.glib.dev}/lib/girepository-1.0:${pkgs.gobject-introspection}/lib/girepository-1.0:${pkgs.gobject-introspection.dev}/lib/girepository-1.0"
-    export LD_LIBRARY_PATH="${pkgs.gtk3}/lib:${pkgs.gtk3.dev}/lib:${pkgs.pango}/lib:${pkgs.pango.dev}/lib:${pkgs.glib}/lib:${pkgs.glib.dev}/lib:${pkgs.cairo}/lib:${pkgs.cairo.dev}/lib:${pkgs.gobject-introspection}/lib:${pkgs.gobject-introspection.dev}/lib"
+    export GI_TYPELIB_PATH="${pkgs.gtk3}/lib/girepository-1.0:${pkgs.pango}/lib/girepository-1.0:${pkgs.glib}/lib/girepository-1.0:${pkgs.gobject-introspection}/lib/girepository-1.0"
+    
+    # Set up library paths
+    export LD_LIBRARY_PATH="${pkgs.gtk3}/lib:${pkgs.pango}/lib:${pkgs.glib}/lib:${pkgs.cairo}/lib"
+    
+    # Set up GSettings schema path
     export XDG_DATA_DIRS="${pkgs.gsettings-desktop-schemas}/share/gsettings-schemas/${pkgs.gsettings-desktop-schemas.name}:${pkgs.gtk3}/share/gsettings-schemas/${pkgs.gtk3.name}:$XDG_DATA_DIRS"
     
-    # Add development headers to PKG_CONFIG_PATH
-    export PKG_CONFIG_PATH="${pkgs.gtk3.dev}/lib/pkgconfig:${pkgs.pango.dev}/lib/pkgconfig:${pkgs.glib.dev}/lib/pkgconfig:${pkgs.cairo.dev}/lib/pkgconfig:${pkgs.gobject-introspection.dev}/lib/pkgconfig:$PKG_CONFIG_PATH"
-    
+    # Set up pkg-config path
+    export PKG_CONFIG_PATH="${pkgs.gtk3.dev}/lib/pkgconfig:${pkgs.pango.dev}/lib/pkgconfig:${pkgs.glib.dev}/lib/pkgconfig:${pkgs.cairo.dev}/lib/pkgconfig"
+
     # Create venv if it doesn't exist
     if [ ! -d "venv" ]; then
       python -m venv venv --system-site-packages
@@ -40,6 +51,7 @@ pkgs.mkShell {
     source venv/bin/activate
     
     # Verify GTK is available
+    echo "Testing GTK availability..."
     python -c "import gi; gi.require_version('Gtk', '3.0'); from gi.repository import Gtk" || echo "Warning: GTK not properly configured"
   '';
 }
